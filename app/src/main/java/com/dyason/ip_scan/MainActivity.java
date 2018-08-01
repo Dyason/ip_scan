03ac3fce03eb0a8ba12e22698a1be3e01f4384db
Jason Lock <P17236416@my365.dmu.ac.uk>
2018-08-01 10:11:52 +0100

    Base for testing

--- app/src/main/java/com/dyason/ip_scan/MainActivity.java
+++ app/src/main/java/com/dyason/ip_scan/MainActivity.java
@@ -1,12 +1,15 @@
 package com.dyason.ip_scan;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.net.*;
 import android.net.wifi.WifiManager;
@@ -15,18 +18,15 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity  {
-
+    public static final String TAG = "YOUR-TAG-NAME";
     TextView info;
     private DhcpInfo d;
     private WifiManager wifii;
     private ConnectivityManager cm;
-
     private Button btnMine;
-
-    public static final String TAG = "YOUR-TAG-NAME";
-
+    private Button btnAsync;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +35,28 @@ public class MainActivity extends AppCompatActivity  {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        btnMine = (Button)findViewById(R.id.mybutton);
-        info = (TextView) findViewById(R.id.dinfo);
-
         //final ScanHosts myscan = new ScanHosts();
         /*btnMine.hasOnClickListeners() {
             //@Override
-            public void onClick() {
+            public void onClick () {
                 myscan.scan(info);
             }
         });*/
 
         setContentView(R.layout.activity_main);
+
+        btnMine = (Button) findViewById(R.id.mybutton);
+        btnMine.setOnClickListener(this);
+
+        btnAsync = (Button) findViewById(R.id.btn2);
+        btnAsync.setOnClickListener(this);
+
+
+        info = (TextView) findViewById(R.id.dinfo);
+        int num1 = 5;
+        float num2 = 2;
+        float test = num1 / num2;
+        info.setText("Testing"+(int) test);
 
         //This get's dhcp details
         wifii = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
@@ -54,7 +64,7 @@ public class MainActivity extends AppCompatActivity  {
         //Get the SSID via this
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        NetInfo getnet = new NetInfo(cm,wifii);
+        NetInfo getnet = new NetInfo(cm, wifii);
 
 
         /*try {
@@ -72,5 +82,137 @@ public class MainActivity extends AppCompatActivity  {
         //ScannerTest scan;
         //checkHosts("10.0.120");
 
+    }
+
+    private class PostTask extends AsyncTask<String, Integer, String> {
+
+        int hostcount;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            displayProgressBar();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String subnet = "10.0.120";
+            final String TAG = "YOUR-TAG-NAME";
+            int hostsfound;
+            int timeout;
+            String url = params[0];
+            hostsfound = 0;
+            timeout = 50;
+
+            try {
+
+                //info.setText("Scanning: Starting");
+                for (int i = 1; i < 255; i++) {
+                    String host = subnet + "." + i;
+                    if (InetAddress.getByName(host).isReachable(timeout)) {
+                        hostsfound++;
+                        publishProgress(i,hostsfound);
+                        //info.setText("Scanning: " + hostsfound + " devices found");
+                        Log.d(TAG, "checkHosts() :: " + host + " is reachable");
+                    } else {
+                        //info.setText("Scanning: Something not right " + i);
+                        publishProgress(i,hostsfound);
+                        Log.d(TAG, "checkHosts() :: " + host + " is unreachable");
+                    }
+                    //info.setText("Finished: Devices found "+hostsfound);
+                }
+            } catch (UnknownHostException e) {
+                Log.d(TAG, "checkHosts() :: UnknownHostException e : " + e);
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.d(TAG, "checkHosts() :: IOException e : " + e);
+                e.printStackTrace();
+            }
+            // Dummy code
+        /*for (int i = 0; i <= 100; i += 5) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            publishProgress(i);
+        }*/
+            return "All Done!";
+
+        }
+        /*protected String doInBackground(String... params) {
+            String url=params[0];
+
+            // Dummy code
+            for (int i = 0; i <= 100; i += 5) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress("Currently:" +i);
+            }
+            return "All Done!";
+        }*/
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            hostcount = 255;
+            double hostpercent = 0.39;
+            double progress = hostpercent * (double) values[0];
+            Log.d(TAG, "Progress: "+hostpercent+" Host: "+values[0]);
+            updateProgressBar((int) progress);
+            info.setText("Progress: "+hostpercent+"Hosts: "+values[1]);
+        }
+        /*protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            if (values != null && values.length > 0) {
+                //Your View attribute object in the activity
+                // already initialized in the onCreate!
+                info.setText(values[0]);
+            }
+        }*/
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            dismissProgressBar();
+        }
+
+        protected void displayProgressBar(){
+            ProgressBar bar=(ProgressBar)findViewById(R.id.progressBar2);
+            bar.setVisibility(View.VISIBLE); //View.INVISIBLE, or View.GONE to hide it.
+        }
+
+        protected void updateProgressBar(Integer... values){
+            ProgressBar bar=(ProgressBar)findViewById(R.id.progressBar2);
+            bar.setProgress(values[0]);
+        }
+
+        protected void dismissProgressBar(){
+            ProgressBar bar=(ProgressBar)findViewById(R.id.progressBar2);
+            bar.setVisibility(View.VISIBLE); //View.INVISIBLE, or View.GONE to hide it.
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        // default method for handling onClick Events..
+        switch (v.getId()) {
+
+            case R.id.mybutton:
+                new PostTask().execute("http://feeds.pcworld.com/pcworld/latestnews");
+                //ScanHosts myscan = new ScanHosts();
+                //myscan.scan(info);
+                break;
+
+            case R.id.btn2:
+                Intent i = new Intent(this, AsyncTaskTestActivity.class);
+                startActivity(i);
+
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/dyason/ip_scan/ScanHosts.java
+++ app/src/main/java/com/dyason/ip_scan/ScanHosts.java
@@ -1,6 +1,7 @@
 package com.dyason.ip_scan;
 
 import android.net.LinkProperties;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
@@ -9,6 +10,8 @@ import android.net.DhcpInfo;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.net.InetAddress;
@@ -19,7 +22,7 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.io.IOException;
 
-public class ScanHosts {
+public class ScanHosts extends AsyncTask<String, Integer, String> {
 
     //var
 
@@ -27,10 +30,50 @@ public class ScanHosts {
     public static final String TAG = "YOUR-TAG-NAME";
     private int hostsfound;
     private int timeout;
+
     //con
     public ScanHosts() {
         hostsfound = 0;
-        timeout = 1;
+        timeout = 50;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String url = params[0];
+
+        try {
+
+            //info.setText("Scanning: Starting");
+            for (int i = 1; i < 255; i++) {
+                String host = subnet + "." + i;
+                if (InetAddress.getByName(host).isReachable(timeout)) {
+                    hostsfound++;
+                    //info.setText("Scanning: " + hostsfound + " devices found");
+                    Log.d(TAG, "checkHosts() :: " + host + " is reachable");
+                } else {
+                    //info.setText("Scanning: Something not right " + i);
+                    Log.d(TAG, "checkHosts() :: " + host + " is unreachable");
+                }
+                //info.setText("Finished: Devices found "+hostsfound);
+            }
+        } catch (UnknownHostException e) {
+            Log.d(TAG, "checkHosts() :: UnknownHostException e : " + e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.d(TAG, "checkHosts() :: IOException e : " + e);
+            e.printStackTrace();
+        }
+        // Dummy code
+        /*for (int i = 0; i <= 100; i += 5) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            publishProgress(i);
+        }*/
+        return "All Done!";
+
     }
 
     public void scan(TextView info) {
@@ -45,7 +88,9 @@ public class ScanHosts {
                     Log.d(TAG, "checkHosts() :: " + host + " is reachable");
                 } else {
                     info.setText("Scanning: Something not right " + i);
+                    Log.d(TAG, "checkHosts() :: " + host + " is unreachable");
                 }
+                info.setText("Finished: Devices found " + hostsfound);
             }
         } catch (UnknownHostException e) {
             Log.d(TAG, "checkHosts() :: UnknownHostException e : " + e);
@@ -56,9 +101,35 @@ public class ScanHosts {
         }
     }
 
-    public int getHostsfound() {
-        return hostsfound;
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        //updateProgressBar(values[0]);
     }
-    //meth
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        //dismissProgressBar();
+    }
+
+    /*protected void displayProgressBar(){
+        ProgressBar bar=(ProgressBar)findViewById(R.id.progressBar2);
+        bar.setVisibility(View.VISIBLE); //View.INVISIBLE, or View.GONE to hide it.
+    }
+
+    protected void updateProgressBar(Integer... values){
+        ProgressBar bar=(ProgressBar)findViewById(R.id.progressBar2);
+        bar.setProgress(values[0]);
+    }
+
+    protected void dismissProgressBar(){
+        ProgressBar bar=(ProgressBar)findViewById(R.id.progressBar2);
+        bar.setVisibility(View.VISIBLE); //View.INVISIBLE, or View.GONE to hide it.
+    }
+    public int getHostsfound() {
+        return hostsfound;*/
 }
+//meth
+
 
